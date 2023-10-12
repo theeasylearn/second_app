75e0fbe46fb0ccec965f4e0912e9680ec734ce2f
theeasylearn <theeasylearn@gmail.com>
2023-10-12 14:25:57 +0530

    sliver

--- lib/sliver_example_1.dart
+++ lib/sliver_example_1.dart
@@ -59,6 +59,7 @@ class SliverExample extends StatelessWidget {
       child: CustomScrollView(
         slivers: [
           SliverAppBar(
+            floating: true, pinned: true,
             title: Text("Sliver example"),
             actions: [
               IconButton(icon: Icon(Icons.home), onPressed: ()  {
@@ -79,14 +80,45 @@ class SliverExample extends StatelessWidget {
                     padding: EdgeInsets.all(10),
                     child: ListTile(
                       leading: CircleAvatar(
-                        child: Image.network("https://cdn.worldvectorlogo.com/logos/jurassic-world-2.svg")
+                        child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Europe_orthographic_Caucasus_Urals_boundary_%28with_borders%29.svg/220px-Europe_orthographic_Caucasus_Urals_boundary_%28with_borders%29.svg.png")
                       ),
                       title: Text(Countries[index].name,textScaleFactor: 1.5,),
                       subtitle: Text(Countries[index].capital),
                     ),
                   ),
                 );
-          },childCount: Countries.length),)
+          },childCount: Countries.length),),
+          SliverGrid(delegate: SliverChildBuilderDelegate((context,index)
+          {
+              return Card(
+                    child: Padding(
+                      padding: EdgeInsets.only(top:20,bottom:10,left: 10),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CircleAvatar(backgroundImage: NetworkImage("https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Europe_orthographic_Caucasus_Urals_boundary_%28with_borders%29.svg/220px-Europe_orthographic_Caucasus_Urals_boundary_%28with_borders%29.svg.png"),),
+                          Padding(
+                            padding: EdgeInsets.only(left:20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(Countries[index].name,textScaleFactor: 1.5,textDirection: TextDirection.ltr,),
+                                Text(Countries[index].capital,textDirection: TextDirection.ltr,),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+              );
+          },childCount: Countries.length),
+
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+              childAspectRatio: 4
+          ),)
         ],
       ),
     );
